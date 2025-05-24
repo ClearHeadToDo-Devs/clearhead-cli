033e1d879a698d2efba565fe4cb3df426a073616
Darrion Burgess <dargondab9@gmail.com>
2025-05-24 01:41:32 -0700

    finally got the first version working

--- src/cliche/main.clj
+++ src/cliche/main.clj
@@ -3,4 +3,6 @@
 (def cli-options {:port {:default 80 :coerce :long} :help {:coerce :boolean}})
 
 (defn -main [& _args]
-  (prn (cli/parse-opts *command-line-args* cli-options)))
+  (prn (cli/parse-opts *command-line-args* {:spec cli-options})))
+
+(comment (cli/parse-opts [:port 700] {:spec cli-options}))
