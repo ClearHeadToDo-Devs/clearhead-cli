a73c8a6c87068af16ab58b6cb1c7406de039ead5
Darrion Burgess <dargondab9@gmail.com>
2025-05-24 22:43:40 -0700

    okay guess this isnt the way we are going after all

--- src/cliche/main.clj
+++
@@ -1,8 +0,0 @@
-(ns cliche.main (:require [babashka.cli :as cli]))
-
-(def cli-options {:port {:default 80 :coerce :long} :help {:coerce :boolean}})
-
-(defn -main [& _args]
-  (prn (cli/parse-opts *command-line-args* {:spec cli-options})))
-
-(comment (cli/parse-opts [:port 700] {:spec cli-options}))
