e8a8db56e7c3e1017be4f14416724edf3f93834b
Darrion Burgess <dargondab9@gmail.com>
2025-07-04 00:51:16 -0700

    finally just went in and implemented the first pass of the implementations just so it stops complaining at me about the annotations

--- src/entities.rs
+++ src/entities.rs
@@ -9,11 +9,11 @@ pub type ActionList = Vec<RootAction>;
 impl TryFrom<TreeWrapper> for ActionList {
     type Error = &'static str;
     fn try_from(value: TreeWrapper) -> Result<Self, Self::Error> {
-        let root_wrapper = create_node_wrapper(value.tree.root_node(), value.source.clone());
+        let root = value.tree.root_node();
         let mut action_list: ActionList = Vec::new();
-        let mut binding = root_wrapper.node.walk();
+        let mut binding = root.walk();
 
-        let root_action_iterator = root_wrapper.node.children(&mut binding);
+        let root_action_iterator = root.children(&mut binding);
 
         root_action_iterator.for_each(|action_node| {
             action_list.push(
@@ -41,8 +41,8 @@ impl<'a> TryFrom<NodeWrapper<'a>> for RootAction {
         let child_iterator = value.node.children(&mut binding);
 
         let mut common = None;
-        let mut story = None;
-        let mut children = None;
+        let mut story: Option<Story> = None;
+        let mut children: Option<ChildActionList> = None;
 
         for child in child_iterator {
             match child.kind() {
@@ -70,13 +70,82 @@ impl<'a> TryFrom<NodeWrapper<'a>> for RootAction {
 }
 type ChildActionList = Vec<ChildAction>;
 
+impl<'a> TryFrom<NodeWrapper<'a>> for ChildActionList {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut child_list: ChildActionList = Vec::new();
+        for child in child_iterator {
+            if child.kind() == "child_action" {
+                let child_wrapper = create_node_wrapper(child, value.source.clone());
+                child_list.push(
+                    child_wrapper
+                        .try_into()
+                        .expect("failed to convert child action"),
+                );
+            }
+        }
+        Ok(child_list)
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct ChildAction {
     common: CommonActionProperties,
     children: Option<GrandChildActionList>,
 }
 
+impl<'a> TryFrom<NodeWrapper<'a>> for ChildAction {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut common = None;
+        let mut children: Option<GrandChildActionList> = None;
+        for child in child_iterator {
+            match child.kind() {
+                "core_action" => {
+                    let core_wrapper = create_node_wrapper(child, value.source.clone());
+                    common = Some(core_wrapper.try_into()?);
+                }
+                "grand_child_actions" => {
+                    children = Some(GrandChildActionList::try_from(create_node_wrapper(
+                        child,
+                        value.source.clone(),
+                    ))?);
+                }
+                _ => {}
+            }
+        }
+        Ok(ChildAction {
+            common: common.ok_or("Missing core action properties")?,
+            children,
+        })
+    }
+}
+
 type GrandChildActionList = Vec<GrandChildAction>;
+
+impl<'a> TryFrom<NodeWrapper<'a>> for GrandChildActionList {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut grand_child_list: GrandChildActionList = Vec::new();
+        for child in child_iterator {
+            if child.kind() == "grand_child_action" {
+                let grand_child_wrapper = create_node_wrapper(child, value.source.clone());
+                grand_child_list.push(
+                    grand_child_wrapper
+                        .try_into()
+                        .expect("failed to convert grand child action"),
+                );
+            }
+        }
+        Ok(grand_child_list)
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct GrandChildAction {
@@ -84,7 +153,56 @@ struct GrandChildAction {
     children: GreatGrandChildActionList,
 }
 
+impl<'a> TryFrom<NodeWrapper<'a>> for GrandChildAction {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut common = None;
+        let mut children: Option<GreatGrandChildActionList> = None;
+        for child in child_iterator {
+            match child.kind() {
+                "core_action" => {
+                    let core_wrapper = create_node_wrapper(child, value.source.clone());
+                    common = Some(core_wrapper.try_into()?);
+                }
+                "great_grand_child_actions" => {
+                    children = Some(GreatGrandChildActionList::try_from(create_node_wrapper(
+                        child,
+                        value.source.clone(),
+                    ))?);
+                }
+                _ => {}
+            }
+        }
+        Ok(GrandChildAction {
+            common: common.ok_or("Missing core action properties")?,
+            children: children.ok_or("Missing great grand child actions")?,
+        })
+    }
+}
+
 type GreatGrandChildActionList = Vec<GreatGrandChildAction>;
+
+impl<'a> TryFrom<NodeWrapper<'a>> for GreatGrandChildActionList {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut great_grand_child_list: GreatGrandChildActionList = Vec::new();
+        for child in child_iterator {
+            if child.kind() == "great_grand_child_action" {
+                let great_grand_child_wrapper = create_node_wrapper(child, value.source.clone());
+                great_grand_child_list.push(
+                    great_grand_child_wrapper
+                        .try_into()
+                        .expect("failed to convert great grand child action"),
+                );
+            }
+        }
+        Ok(great_grand_child_list)
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct GreatGrandChildAction {
@@ -92,7 +210,56 @@ struct GreatGrandChildAction {
     children: Option<GreatGreatGrandChildActionList>,
 }
 
+impl<'a> TryFrom<NodeWrapper<'a>> for GreatGrandChildAction {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut common = None;
+        let mut children: Option<GreatGreatGrandChildActionList> = None;
+        for child in child_iterator {
+            match child.kind() {
+                "core_action" => {
+                    let core_wrapper = create_node_wrapper(child, value.source.clone());
+                    common = Some(core_wrapper.try_into()?);
+                }
+                "great_great_grand_child_actions" => {
+                    children = Some(GreatGreatGrandChildActionList::try_from(
+                        create_node_wrapper(child, value.source.clone()),
+                    )?);
+                }
+                _ => {}
+            }
+        }
+        Ok(GreatGrandChildAction {
+            common: common.ok_or("Missing core action properties")?,
+            children: children,
+        })
+    }
+}
+
 type GreatGreatGrandChildActionList = Vec<GreatGreatGrandChildAction>;
+
+impl<'a> TryFrom<NodeWrapper<'a>> for GreatGreatGrandChildActionList {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut great_great_grand_child_list: GreatGreatGrandChildActionList = Vec::new();
+        for child in child_iterator {
+            if child.kind() == "great_great_grand_child_action" {
+                let great_great_grand_child_wrapper =
+                    create_node_wrapper(child, value.source.clone());
+                great_great_grand_child_list.push(
+                    great_great_grand_child_wrapper
+                        .try_into()
+                        .expect("failed to convert great great grand child action"),
+                );
+            }
+        }
+        Ok(great_great_grand_child_list)
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct GreatGreatGrandChildAction {
@@ -100,11 +267,78 @@ struct GreatGreatGrandChildAction {
     children: Option<LeafActionList>,
 }
 
+impl<'a> TryFrom<NodeWrapper<'a>> for GreatGreatGrandChildAction {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut common = None;
+        let mut children: Option<LeafActionList> = None;
+        for child in child_iterator {
+            match child.kind() {
+                "core_action" => {
+                    let core_wrapper = create_node_wrapper(child, value.source.clone());
+                    common = Some(core_wrapper.try_into()?);
+                }
+                "leaf_actions" => {
+                    children = Some(LeafActionList::try_from(create_node_wrapper(
+                        child,
+                        value.source.clone(),
+                    ))?);
+                }
+                _ => {}
+            }
+        }
+        Ok(GreatGreatGrandChildAction {
+            common: common.ok_or("Missing core action properties")?,
+            children,
+        })
+    }
+}
+
 type LeafActionList = Vec<LeafAction>;
+
+impl<'a> TryFrom<NodeWrapper<'a>> for LeafActionList {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut leaf_list: LeafActionList = Vec::new();
+        for child in child_iterator {
+            if child.kind() == "leaf_action" {
+                let leaf_wrapper = create_node_wrapper(child, value.source.clone());
+                leaf_list.push(
+                    leaf_wrapper
+                        .try_into()
+                        .expect("failed to convert leaf action"),
+                );
+            }
+        }
+        Ok(leaf_list)
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct LeafAction {
     common: CommonActionProperties,
+}
+
+impl<'a> TryFrom<NodeWrapper<'a>> for LeafAction {
+    type Error = &'static str;
+    fn try_from(value: NodeWrapper<'a>) -> Result<Self, Self::Error> {
+        let mut binding = value.node.walk();
+        let child_iterator = value.node.children(&mut binding);
+        let mut common = None;
+        for child in child_iterator {
+            if child.kind() == "core_action" {
+                let core_wrapper = create_node_wrapper(child, value.source.clone());
+                common = Some(core_wrapper.try_into()?);
+            }
+        }
+        Ok(LeafAction {
+            common: common.ok_or("Missing core action properties")?,
+        })
+    }
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct CommonActionProperties {
