fc4ba6dd623ade05ef631a12b82d0e1f8eaf3c68
Darrion Burgess <dargondab9@gmail.com>
2025-07-07 00:05:37 -0700

    adding final touch to handling singe-serving move to map

--- src/argparser.rs
+++ src/argparser.rs
@@ -8,10 +8,8 @@ type Args = Map<String, Value>;
 pub fn get_cli_map() -> Result<Args, String> {
     let cli = Cli::parse();
 
-    let value = serde_json::to_value(cli)
-        .map_err(|e| format!("unable to translate cli args to a json value {}", e))?;
-
-    value
+    serde_json::to_value(cli)
+        .map_err(|e| format!("unable to translate cli args to a json value {}", e))?
         .as_object()
         .cloned()
         .ok_or("Failed to convert CLI args to object".to_string())
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,21 @@
-use std::collections::HashMap;
-
-use serde_json::Value;
+use serde_json::{Map, Value};
 use tree_sitter::Tree;
 
 pub mod treesitter;
 
 pub mod entities;
-use entities::{ActionList, ActionState};
-
-use uuid::Uuid;
-
-use tree_sitter::Node;
+use entities::ActionList;
 
 // merging json hashmaps as our universal structure
-pub fn merge_hashmaps(left: &Value, right: &Value) -> Result<Value, String> {
-    if left.is_object() && right.is_object() {
-        let mut merged = left.as_object().unwrap().clone();
-        for (key, value) in right.as_object().unwrap() {
-            merged.insert(key.clone(), value.clone());
-        }
-        Ok(Value::Object(merged))
-    } else {
-        return Err("Both values must be JSON objects".to_string());
+pub fn merge_hashmaps(
+    left: &Map<String, Value>,
+    right: &Map<String, Value>,
+) -> Result<Value, String> {
+    let mut merged = left.clone();
+    for (key, value) in right {
+        merged.insert(key.clone(), value.clone());
     }
+    Ok(Value::Object(merged))
 }
 
 pub fn get_action_list_struct(_opts: &Value, actions: &str) -> Result<ActionList, String> {
@@ -62,4 +55,3 @@ fn get_action_list_tree(actions: &str) -> Result<Tree, String> {
         .parse(actions, None)
         .ok_or("Failed to parse tree".to_string())
 }
-
