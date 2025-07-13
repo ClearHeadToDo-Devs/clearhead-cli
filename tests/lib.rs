df5b4fc32b0727987d7ffa9c6ab871243efedcd7
Darrion Burgess <dargondab9@gmail.com>
2025-07-13 16:55:12 -0700

    finally got my first test pulling from the repo!!!

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,6 @@
 use cliche::entities::*;
 use cliche::*;
-use reqwest::blocking::get;
+use reqwest::blocking::*;
 use std::collections::HashMap;
 
 #[test]
@@ -29,15 +29,14 @@ fn convert_basic_action() {
 }
 #[test]
 fn convert_basic_action_from_examples() {
-    let test_action =
-        get_test_examples().unwrap()["children"]["minimal_example"]["content"].clone();
+    let test_action = get_test_examples().unwrap()["children"]["minimal"]["content"].clone();
     let test_config_str = r#"{}"#;
     let test_config = serde_json::from_str(test_config_str).expect("unable to conver");
 
     let derived_struct = get_action_list_struct(&test_config, &test_action).unwrap();
     let expected_struct = vec![RootAction {
         common: CommonActionProperties {
-            state: ActionState::Completed,
+            state: ActionState::NotStarted,
             name: "test".to_string(),
             description: None,
             priority: None,
@@ -55,9 +54,11 @@ fn convert_basic_action_from_examples() {
 
 fn get_test_examples() -> Result<HashMap<String, HashMap<String, HashMap<String, String>>>, String>
 {
+    let request_client = Client::new();
     //first, we get the list of test files, their names, and descriptions directly from the GH repo
-    let list_get_response = get("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/test/test_descriptions.json").expect("unable to get url in question").text().expect("unable to get text from request");
-    let binding: serde_json::Value = serde_json::from_str(&list_get_response).expect("could not parse JSON");
+    let list_get_response = request_client.get("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/test/test_descriptions.json").send().expect("unable to get url in question").text().expect("unable to get text from request");
+    let binding: serde_json::Value =
+        serde_json::from_str(&list_get_response).expect("could not parse JSON");
     let test_list = binding.as_object().expect("couldnt get map out of this");
 
     // Then we create the hashmap we will ultimately return
@@ -67,7 +68,7 @@ fn get_test_examples() -> Result<HashMap<String, HashMap<String, HashMap<String,
         let mut file_tests: HashMap<String, HashMap<String, String>> = HashMap::new();
         for (test_name, description) in tests.as_object().expect("not a child object") {
             //get the example file that corresponds to the test name
-            let test_file_contents = get(format!("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/examples/{}.actions",test_name)).expect("couldnt find example file").text().expect("unable to translate the body");
+            let test_file_contents = request_client.get(format!("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/examples/{}.actions",test_name)).send().expect("couldnt find example file").text().expect("unable to translate the body");
             file_tests.insert(
                 test_name.to_string(),
                 HashMap::from([
