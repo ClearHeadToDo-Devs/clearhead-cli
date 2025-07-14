05bbaff273904667c78f57d71ac57989922a91e8
Darrion Burgess <dargondab9@gmail.com>
2025-07-14 00:05:25 -0700

    finally got the dynamic generation and import working...

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,6 +2,7 @@ use cliche::entities::*;
 use cliche::*;
 use reqwest::blocking::*;
 use std::collections::HashMap;
+use tree_sitter_actions::get_test_data;
 
 #[test]
 fn convert_basic_action() {
@@ -29,7 +30,7 @@ fn convert_basic_action() {
 }
 #[test]
 fn convert_basic_action_from_examples() {
-    let test_action = get_test_examples().unwrap()["children"]["minimal"]["content"].clone();
+    let test_action = get_test_data()["children"]["minimal"]["content"].clone();
     let test_config_str = r#"{}"#;
     let test_config = serde_json::from_str(test_config_str).expect("unable to conver");
 
@@ -50,34 +51,4 @@ fn convert_basic_action_from_examples() {
     }];
 
     assert_eq!(derived_struct, expected_struct);
-}
-
-fn get_test_examples() -> Result<HashMap<String, HashMap<String, HashMap<String, String>>>, String>
-{
-    let request_client = Client::new();
-    //first, we get the list of test files, their names, and descriptions directly from the GH repo
-    let list_get_response = request_client.get("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/test/test_descriptions.json").send().expect("unable to get url in question").text().expect("unable to get text from request");
-    let binding: serde_json::Value =
-        serde_json::from_str(&list_get_response).expect("could not parse JSON");
-    let test_list = binding.as_object().expect("couldnt get map out of this");
-
-    // Then we create the hashmap we will ultimately return
-    let mut files: HashMap<String, HashMap<String, HashMap<String, String>>> = HashMap::new();
-
-    for (filename, tests) in test_list {
-        let mut file_tests: HashMap<String, HashMap<String, String>> = HashMap::new();
-        for (test_name, description) in tests.as_object().expect("not a child object") {
-            //get the example file that corresponds to the test name
-            let test_file_contents = request_client.get(format!("https://raw.githubusercontent.com/ClearHeadToDo-Devs/tree-sitter-actions/refs/heads/master/examples/{}.actions",test_name)).send().expect("couldnt find example file").text().expect("unable to translate the body");
-            file_tests.insert(
-                test_name.to_string(),
-                HashMap::from([
-                    ("descripition".to_string(), description.to_string()),
-                    ("content".to_string(), test_file_contents),
-                ]),
-            );
-        }
-        files.insert(filename.to_string(), file_tests);
-    }
-    Ok(files)
 }
